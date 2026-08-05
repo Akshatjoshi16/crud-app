2c8c5e3e53d41232b51f253d8ba5441171b5a330
Akshatjoshi16 <akshatjoshii16@gmail.com>
2026-08-05 22:54:49 +0530

    Added controller-service-repository architecture

--- src/main/java/com/app/model/Person.java
+++ src/main/java/com/app/model/Person.java
@@ -22,4 +22,18 @@ public class Person {
         this.name=name;
         this.age=age;
     }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return this.age;
+    }
 }
